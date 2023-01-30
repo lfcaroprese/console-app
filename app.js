524bb424b435e06927d15a4d92e8cdf8ddac30d8
Fabricio Caroprese <35928006+lfcaroprese@users.noreply.github.com>
2023-01-29 23:12:44 -0300

    Se creo el archivo messages.js que contiene funciones relacionadas al menú y su interacción con el usuario

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/messages');
+
 console.clear();
 
 const main = async() =>{
     console.log('Hola Mundo');
+
+    mostrarMenu();
+    //pausa();
 }
 
 main();
